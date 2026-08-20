ab53ba2793947e1c06cfb854dca910081a06ac40
ryancursinom <rcursinomoraes@gmail.com>
2026-08-20 07:54:24 -0300

    feat(spring-project): Criação da arquitetura base do backend

---
+++ src/main/java/com/example/rhpicpaybackend/RhPicpayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhpicpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhPicpayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhPicpayBackendApplication.class, args);
+    }
+
+}
