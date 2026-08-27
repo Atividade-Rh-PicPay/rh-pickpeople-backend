edea56444bdcd010ac90474f0e89f632ae12b16f
Caio Marcos <caio.maciel@germinare.org.br>
2026-08-27 10:43:15 -0300

    feat: making some changes in authentication response content

--- src/main/java/com/example/rhpicpaybackend/auth/controller/AuthController.java
+++ src/main/java/com/example/rhpicpaybackend/auth/controller/AuthController.java
@@ -5,13 +5,18 @@ import com.example.rhpicpaybackend.auth.dto.input.RefreshTokenInputDTO;
 import com.example.rhpicpaybackend.auth.dto.output.LoginOutputDTO;
 import com.example.rhpicpaybackend.auth.dto.request.LoginRequestDTO;
 import com.example.rhpicpaybackend.auth.service.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,12 +28,32 @@ public class AuthController {
   public ResponseEntity<LoginOutputDTO> login(
       @RequestBody
       @Valid
-      LoginRequestDTO input
+      LoginRequestDTO input,
+
+      HttpServletResponse response
   ){
+    LoginOutputDTO output = this.authService.login(
+        new LoginInputDTO(input)
+    );
+
+    ResponseCookie cookie = ResponseCookie.from(
+            "access_token",
+            output.accessToken()
+        )
+        .httpOnly(true)
+        .secure(true)
+        .sameSite("None")
+        .path("/")
+        .maxAge(Duration.ofHours(1))
+        .build();
+
+    response.addHeader(
+        HttpHeaders.SET_COOKIE,
+        cookie.toString()
+    );
+
     return new ResponseEntity<>(
-        this.authService.login(
-            new LoginInputDTO(input)
-        ),
+        output,
         HttpStatus.OK
     );
   }
@@ -42,12 +67,32 @@ public class AuthController {
 
       @RequestHeader("Authorization")
       @NotEmpty(message = "{validation.refresh-token.required}")
-      String refreshToken
+      String refreshToken,
+
+      HttpServletResponse response
   ){
+    LoginOutputDTO output = this.authService.refreshToken(
+        new RefreshTokenInputDTO(email, refreshToken)
+    );
+
+    ResponseCookie cookie = ResponseCookie.from(
+            "access_token",
+            output.accessToken()
+        )
+        .httpOnly(true)
+        .secure(true)
+        .sameSite("None")
+        .path("/")
+        .maxAge(Duration.ofHours(1))
+        .build();
+
+    response.addHeader(
+        HttpHeaders.SET_COOKIE,
+        cookie.toString()
+    );
+
     return new ResponseEntity<>(
-        this.authService.refreshToken(
-            new RefreshTokenInputDTO(email, refreshToken)
-        ),
+        output,
         HttpStatus.OK
     );
   }
